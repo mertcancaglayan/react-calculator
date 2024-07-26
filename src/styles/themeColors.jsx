19928ec043deffbd1cfab6411e7a0e2fab4bfdd1
M.can <caglayan.mertcan37@gmail.com>
2024-07-26 16:10:42 +0300

    add theme colors

---
+++ src/styles/themeColors.jsx
@@ -0,0 +1,22 @@
+export const themeColors = [
+	{
+		light: {
+			toggleButtonLight: "rgb(226, 236, 253)",
+			toggleButtonDark: "rgb(195, 215, 251)",
+			containerBackground: "rgb(245, 245, 245)",
+			primaryTextColor: "rgb(149, 149, 149)",
+			commonTextColor: "rgb(55, 55, 55)",
+			operatorsContainerColor: "linear-gradient(to right, rgb(201, 208, 241), rgb(178, 205, 250))",
+		},
+	},
+	{
+		dark: {
+			toggleButtonLight: "rgb(78 99 152)",
+			toggleButtonDark: "rgb(38 48 95)",
+			containerBackground: "rgb(36, 37, 39)",
+			primaryTextColor: "rgb(141, 142, 143)",
+			commonTextColor: "rgb(250, 250, 250)",
+			operatorsContainerColor: "linear-gradient(to right, rgb(55, 69, 105), rgb(32, 39, 65))",
+		},
+	},
+];
