1442253bb41abb7386e2f6af5298429f64ec7489
M.can <caglayan.mertcan37@gmail.com>
2024-07-26 16:30:44 +0300

    add themeContext

---
+++ src/context/themeContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from "react";
+import { themeColors } from "../styles/themeColors";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+	const [theme, setTheme] = useState("light");
+
+	const toggleTheme = () => {
+		setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+	};
+
+	const currentThemeColors = themeColors[theme];
+
+	return <ThemeContext.Provider value={{ theme, toggleTheme, currentThemeColors }}>{children}</ThemeContext.Provider>;
+};
+
+export const useTheme = () => useContext(ThemeContext);
