78a1085fd56216be955eeed953a1c5aac5682850
M.can <caglayan.mertcan37@gmail.com>
2024-07-28 13:04:23 +0300

    add calculations

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Toggle from "./components/toggle/Toggle";
 import "./Calculator.css";
 import { useTheme } from "./context/themeContext";
 import Display from "./components/display/Display";
 import Keypad from "./components/keypad/Keypad";
+import { calculate } from "./func/calculate";
 
 function Calculator() {
 	const { currentThemeColors } = useTheme();
 
+	const [displayValue, setDisplayValue] = useState("");
+	const [resultValue, setResultValue] = useState("");
+
 	const handleButtonClick = (value) => {
-		console.log("Button clicked:", value);
+		if (value === "=") {
+			const result = calculate(value, displayValue);
+			setResultValue(result);
+		} else {
+			const newExpression = calculate(value, displayValue);
+			setDisplayValue(newExpression);
+			setResultValue("");
+		}
 	};
 
 	return (
 		<div className="calculator" style={{ backgroundColor: currentThemeColors.calculatorBackground }}>
-			<Toggle></Toggle>
-			<Display></Display>
-			<Keypad onButtonClick={handleButtonClick}></Keypad>
+			<Toggle />
+			<Display displayValue={displayValue} resultValue={resultValue} />
+			<Keypad onButtonClick={handleButtonClick} />
 		</div>
 	);
 }
--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import "./Display.css";
 import { useTheme } from "../../context/themeContext";
 
-function Display() {
+function Display({displayValue , resultValue}) {
 	const { currentThemeColors } = useTheme();
 
 	return (
 		<div className="displayContainer">
 			<div className="equation" style={{ color: currentThemeColors.primaryTextColor }}>
-				1.000 × 4 + 2 × 1
+				{displayValue}
 			</div>
 			<div className="result" style={{ color: currentThemeColors.commonTextColor }}>
 				<div className="resultEqual">=</div>
-				<div className="resultValue">4002</div>
+				<div className="resultValue">{resultValue}</div>
 			</div>
 		</div>
 	);
